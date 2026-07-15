97851c69b8d8205575215194ba759f4abd3aae08
aryavnshiakhilesharthkirti-gif <aryavnshiakhilesharthkirti@gmail.com>
2026-07-15 22:54:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import CurrentWeather from './components/CurrentWeather';
 import Forecast from './components/Forecast';
 import './App.css';
 
-const API_KEY ='bd55ab25501512db3fbe773dffc51568
-    ';
+const API_KEY ='bd55ab25501512db3fbe773dffc51568'
 
 function App() {
   const [city, setCity] = useState('London');
